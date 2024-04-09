11addbe7659206cd4ae78f9cb9f54bad9a153410
Vladyslav <dr.tienam123@gmail.com>
2024-04-09 08:16:34 +0300

    Добавил алиас, описал в документации

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import {resolve} from 'path';
 
 export default defineConfig(({ command }) => {
   return {
@@ -26,5 +27,10 @@ export default defineConfig(({ command }) => {
       outDir: '../dist',
     },
     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
   };
 });

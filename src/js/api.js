65fde1ca41344b6933213141d433dd1f53a2e82a
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-04-10 20:42:55 +0300

    work-together-js

--- src/js/api.js
+++ src/js/api.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
-function postData() {
-axios.post()
+function postData(emailInput, commentsInput) {
+  axios
+    .post('https://portfolio-js.b.goit.study/api-docs/requests', {
+      email: emailInput,
+      comments: commentsInput,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-async function getData() {
-
-}
+async function getData() {}
---
+++ src/js/work-together.js
@@ -0,0 +1,18 @@
+// import { postData } from './api';
+const buttonEl = document.querySelector('.skills-button');
+const inputEmail = document.querySelector('input#user-email');
+const inputComments = document.querySelector('input#user-comments');
+// const valueInput = inputEl.value;
+
+let emailInput;
+let commentsInput;
+
+buttonEl.addEventListener('submit', e => {
+  e.preventDefault();
+  emailInput = inputEmail.value;
+  commentsInput = commentsInput.value;
+  postData(emailInput, commentsInput);
+  console.log(emailInput);
+  console.log(commentsInput);
+  console.log(postData(emailInput, commentsInput));
+});

aa9b9aaf03f059549df270a5b9cac81cc37096e6
Vladyslav <dr.tienam123@gmail.com>
2024-04-10 10:06:42 +0300

    changed animation start with Observer

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,14 +1,22 @@
-const coverBlock = document.querySelector('.content');
-const images = document.querySelectorAll('.marquee__line');
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return rect.y < 0
-}
+(function() {
+  const coverBlock = document.querySelector('.content');
+  const images = document.querySelectorAll('.marquee__line');
 
-window.addEventListener('scroll',(e) => {
-  if (isElementInViewport(coverBlock)) {
-    images.forEach(el => el.classList.add('animate-block'))
-  } else {
-    images.forEach(el => el.classList.remove('animate-block'))
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        images.forEach(image => image.classList.add('animate-block'));
+      } else {
+        images.forEach(image => image.classList.remove('animate-block'));
+      }
+    });
   }
-})
+
+  let options = {
+    threshold: [0.5],
+  };
+  let observer = new IntersectionObserver(onEntry, options);
+  observer.observe(coverBlock);
+
+})();
+

fcc9ee158f1b45d400c13b0356723b3bbab62956
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-04-11 10:26:02 +0300

    fixes WiP

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -36,15 +36,32 @@ new Accordion('.accordion-container', {
   onOpen: function (currentElement) {
     console.log(currentElement);
     console.log('currentElement is open');
-    currentElement.querySelector(
-      'svg.ac-icon'
-    ).innerHTML = `<path d="M15 12.5L10 7.5L5 12.5" stroke="#FAFAFA" stroke-width="2" />`;
+    // currentElement.querySelector('svg.ac-icon').innerHTML = `<path d="M15 12.5L10 7.5L5 12.5" stroke="#FAFAFA" stroke-width="2" />`;
+    currentElement.querySelector('svg.ac-icon').classList.add('active');
   },
   onClose: function (currentElement) {
     console.log(currentElement);
     console.log('currentElement is closed');
-    currentElement.querySelector(
-      'svg.ac-icon'
-    ).innerHTML = `<path d="M15 7.5L10 12.5L5 7.5" stroke="#FAFAFA" stroke-width="2" />`;
+    // currentElement.querySelector('svg.ac-icon').innerHTML = `<path d="M15 7.5L10 12.5L5 7.5" stroke="#FAFAFA" stroke-width="2" />`;
+    currentElement.querySelector('svg.ac-icon').classList.remove('active');
   },
 });
+
+
+// const activeContent = document.querySelector('#' + item.dataset.tab);
+
+// if (activeContent.classList.contains('active')) {
+//   activeContent.classList.remove('active');
+
+//   activeContent.style.maxHeight = 0;
+// } else {
+//   contents.forEach(element => {
+//     element.classList.remove('active');
+//     element.style.maxHeight = 0;
+//   })
+//   titles.forEach(element => element.classList.remove('active'));
+
+//   item.classList.add('active');
+//   activeContent.classList.add('active');
+//   activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+// }

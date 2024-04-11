63a9ac493a23003716658b0e84668ad158d5d798
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-04-11 12:58:32 +0300

    Правки внесены
    - Реализация стрелок через спрайты, как в FAQ;
    - Удалил стили со вайпер слайда;
    - Соседняя секция skills больше не налазит на страницу;
    - Адаптация текстов под цветотемы

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -37,13 +37,13 @@ new Accordion('.accordion-container', {
     console.log(currentElement);
     console.log('currentElement is open');
     // currentElement.querySelector('svg.ac-icon').innerHTML = `<path d="M15 12.5L10 7.5L5 12.5" stroke="#FAFAFA" stroke-width="2" />`;
-    currentElement.querySelector('svg.ac-icon').classList.add('active');
+    currentElement.querySelector('svg.ac-icon').classList.add('ac-icon-active');
   },
   onClose: function (currentElement) {
     console.log(currentElement);
     console.log('currentElement is closed');
     // currentElement.querySelector('svg.ac-icon').innerHTML = `<path d="M15 7.5L10 12.5L5 7.5" stroke="#FAFAFA" stroke-width="2" />`;
-    currentElement.querySelector('svg.ac-icon').classList.remove('active');
+    currentElement.querySelector('svg.ac-icon').classList.remove('ac-icon-active');
   },
 });
 

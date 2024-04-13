f96365e9e0ca0b887fb076ad51a0f5e180bfb0d6
Zhanna14 <tkachukzhanna25@gmail.com>
2024-04-13 13:05:09 +0200

    try2

--- src/js/skills-list.js
+++ src/js/skills-list.js
@@ -1,25 +1,47 @@
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
+import 'swiper/css/scrollbar';
 
-const skillButtons = document.querySelectorAll('.skill-list');
-let number = 0;
-let mySwiper = null; //
 
-mySwiper = new Swiper('.swiper-skills-list', {
+const mySwiper = new Swiper('.swiper-skills-list', {
+  initialSlide: -1,
   modules: [Navigation],
-  spaceBetween:20,
-  slidesPerView:2,
-  loop:true,
+  direction: 'horizontal',
+  loop: true,
+  // centeredSlides: true,
   breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
     768: {
-      slidesPerView:3,
-      spaceBetween:"50px"
-    } ,
+      slidesPerView: 3,
+      centeredSlides: true,
+    },
     1440: {
-      slidesPerView: 6,
-    }
+      slidesPerView: 5,
+    },
   },
   navigation: {
-    nextEl: '.swiper-button-next',
+    nextEl: '.skills-arrow',
   },
+  // mousewheel: {
+  //   invert: true,
+  // },
 });
+  
+
+  // mySwiper.on('slideChange', function () {
+  //   const activeSlide = document.querySelector(
+  //     '.swiper-slide-active .skills-button'
+  //   );
+  //   const activeButton = document.querySelector('.skills-button-active');
+
+  //   if (activeButton) {
+  //     activeButton.classList.remove('skills-button-active');
+  //   }
+
+  //   if (activeSlide) {
+  //     activeSlide.classList.add('skills-button-active');
+  //   }
+  // });
+

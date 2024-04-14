60ed4e4d98c1fdafb23220aa662ca12ce14c7518
Vladyslav <dr.tienam123@gmail.com>
2024-04-14 06:51:37 +0300

    img optimizq 70%

--- vite.config.js
+++ vite.config.js
@@ -14,19 +14,19 @@ const DEFAULT_OPTIONS = {
   ansiColors: true,
   png: {
     // https://sharp.pixelplumbing.com/api-output#png
-    quality: 80,
+    quality: 70,
   },
   jpeg: {
     // https://sharp.pixelplumbing.com/api-output#jpeg
-    quality: 80,
+    quality: 70,
   },
   jpg: {
     // https://sharp.pixelplumbing.com/api-output#jpeg
-    quality: 80,
+    quality: 70,
   },
   tiff: {
     // https://sharp.pixelplumbing.com/api-output#tiff
-    quality: 80,
+    quality: 70,
   },
   // gif does not support lossless compression
   // https://sharp.pixelplumbing.com/api-output#gif

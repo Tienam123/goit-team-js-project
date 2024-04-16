e0520896cd96be92c81c2f15baaaeadaa1630c36
Zhanna14 <tkachukzhanna25@gmail.com>
2024-04-16 21:27:04 +0200

    145

--- src/js/skills-list.js
+++ src/js/skills-list.js
@@ -2,46 +2,64 @@ import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/scrollbar';
 
+const swiperWrapper = document.querySelector('.swiper-wrapper');
+const slides = document.querySelectorAll('.swiper-slide');
+const additionalSlidesCount = 12;
+for (let i = 0; i < additionalSlidesCount; i++) {
+  const clonedSlide = slides[i].cloneNode(true);
+  swiperWrapper.appendChild(clonedSlide);
+}
 
 const mySwiper = new Swiper('.swiper-skills-list', {
-  initialSlide: -1,
+  initialSlide: 0,
   modules: [Navigation],
   direction: 'horizontal',
   loop: true,
-  // centeredSlides: true,
+  spaceBetween: 0,
   breakpoints: {
     320: {
       slidesPerView: 2,
     },
     768: {
       slidesPerView: 3,
-      centeredSlides: true,
     },
     1440: {
-      slidesPerView: 5,
+      slidesPerView: 6,
     },
   },
   navigation: {
     nextEl: '.skills-arrow',
   },
-  // mousewheel: {
-  //   invert: true,
-  // },
+  watchOverflow: true,
+  mousewheel: {
+    invert: true,
+  },
+  on: {
+    init: function () {
+      const swiperSlides = this.slides;
+      for (let i = 0; i < swiperSlides.length; i++) {
+        swiperSlides[i].style.width = 'auto';
+      }
+
+    //   const activeIndex = this.activeIndex;
+    //   swiperSlides.forEach((slide, index) => {
+    //     if (index === activeIndex) {
+    //       slide.classList.add('active-slide');
+    //     } else {
+    //       slide.classList.remove('active-slide');
+    //     }
+    //   });
+    // },
+    // slideChange: function () {
+    //   const swiperSlides = this.slides;
+    //   const activeIndex = this.activeIndex;
+    //   swiperSlides.forEach((slide, index) => {
+    //     if (index === activeIndex) {
+    //       slide.classList.add('active-slide');
+    //     } else {
+    //       slide.classList.remove('active-slide');
+    //     }
+    //   });
+    },
+  },
 });
-  
-
-  // mySwiper.on('slideChange', function () {
-  //   const activeSlide = document.querySelector(
-  //     '.swiper-slide-active .skills-button'
-  //   );
-  //   const activeButton = document.querySelector('.skills-button-active');
-
-  //   if (activeButton) {
-  //     activeButton.classList.remove('skills-button-active');
-  //   }
-
-  //   if (activeSlide) {
-  //     activeSlide.classList.add('skills-button-active');
-  //   }
-  // });
-
